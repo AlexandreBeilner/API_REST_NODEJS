4bb2d02fcdc437474c78d618e1d74e24128927a1
Alexandre Beilner <134701937+AlexandreBeilner@users.noreply.github.com>
2024-02-23 15:18:55 -0300

    Delete build directory

--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-require("dotenv/config");
-server_1.server.listen(process.env.PORT, () => {
-    console.log(`App rodando na porta ${process.env.PORT}`);
-});
